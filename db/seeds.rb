93e5d9788232420a0d1a9fc3d7664d490d20accc
carlosprietobarron <carlosprietobarron@outlook.com>
2021-06-15 15:32:43 -0500

    Feed data to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,144 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Indicator.create(name: 'Cold Calls', goal: 20)
+#Indicator.create(name: 'Cold Calls', goal: 20)
 
-Indicator.create(name: 'Registered Apointments', goal: 10)
+Measure.create(day: "03-02-2021", measure: "21", comentary: "feeded test data", indicator_id: "1")
+Measure.create(day: "04-02-2021", measure: "15", comentary: "feeded test data", indicator_id: "1")
+Measure.create(day: "05-02-2021", measure: "17", comentary: "feeded test data", indicator_id: "1")
+Measure.create(day: "06-02-2021", measure: "21", comentary: "feeded test data", indicator_id: "1")
+Measure.create(day: "07-02-2021", measure: "23", comentary: "feeded test data", indicator_id: "1")
+Measure.create(day: "08-02-2021", measure: "18", comentary: "feeded test data", indicator_id: "1")
+Measure.create(day: "09-02-2021", measure: "14", comentary: "feeded test data", indicator_id: "1")
 
-Indicator.create(name: 'Sales Visits', goal: 5)
 
-Indicator.create(name: 'Demo Meeting', goal: 5)
+Measure.create(day: "10-03-2021", measure: "10", comentary: "feeded test data", indicator_id: "1")
+Measure.create(day: "11-03-2021", measure: "5", comentary: "feeded test data", indicator_id: "1")
+Measure.create(day: "12-03-2021", measure: "7", comentary: "feeded test data", indicator_id: "1")
+Measure.create(day: "13-03-2021", measure: "20", comentary: "feeded test data", indicator_id: "1")
+Measure.create(day: "15-03-2021", measure: "17", comentary: "feeded test data", indicator_id: "1")
+Measure.create(day: "16-03-2021", measure: "18", comentary: "feeded test data", indicator_id: "1")
+Measure.create(day: "17-03-2021", measure: "15", comentary: "feeded test data", indicator_id: "1")
 
-Indicator.create(name: 'Qoutes Issued', goal: 20)
 
-if Rails.env.development?
-  AdminUser.create!(email: 'admin@example.com', password: 'password',
-                    password_confirmation: 'password')
-end
+Measure.create(day: "20-04-2021", measure: "10", comentary: "feeded test data", indicator_id: "1")
+Measure.create(day: "21-04-2021", measure: "17", comentary: "feeded test data", indicator_id: "1")
+Measure.create(day: "22-04-2021", measure: "17", comentary: "feeded test data", indicator_id: "1")
+Measure.create(day: "23-04-2021", measure: "20", comentary: "feeded test data", indicator_id: "1")
+Measure.create(day: "24-04-2021", measure: "21", comentary: "feeded test data", indicator_id: "1")
+Measure.create(day: "25-04-2021", measure: "2", comentary: "feeded test data", indicator_id: "1")
+Measure.create(day: "26-04-2021", measure: "1", comentary: "feeded test data", indicator_id: "1")
+
+#Indicator.create(name: 'Registered Apointments', goal: 10)
+
+Measure.create(day: "20-01-2021", measure: "10", comentary: "feeded test data", indicator_id: "2")
+Measure.create(day: "21-01-2021", measure: "17", comentary: "feeded test data", indicator_id: "2")
+Measure.create(day: "22-01-2021", measure: "17", comentary: "feeded test data", indicator_id: "2")
+Measure.create(day: "23-01-2021", measure: "20", comentary: "feeded test data", indicator_id: "2")
+Measure.create(day: "24-01-2021", measure: "21", comentary: "feeded test data", indicator_id: "2")
+Measure.create(day: "25-01-2021", measure: "2", comentary: "feeded test data", indicator_id: "2")
+Measure.create(day: "26-01-2021", measure: "2", comentary: "feeded test data", indicator_id: "2")
+
+Measure.create(day: "03-03-2021", measure: "21", comentary: "feeded test data", indicator_id: "2")
+Measure.create(day: "04-03-2021", measure: "15", comentary: "feeded test data", indicator_id: "2")
+Measure.create(day: "05-03-2021", measure: "17", comentary: "feeded test data", indicator_id: "2")
+Measure.create(day: "06-03-2021", measure: "21", comentary: "feeded test data", indicator_id: "2")
+Measure.create(day: "07-03-2021", measure: "23", comentary: "feeded test data", indicator_id: "2")
+Measure.create(day: "08-03-2021", measure: "18", comentary: "feeded test data", indicator_id: "2")
+Measure.create(day: "09-03-2021", measure: "14", comentary: "feeded test data", indicator_id: "2")
+Measure.create(day: "10-03-2021", measure: "21", comentary: "feeded test data", indicator_id: "2")
+Measure.create(day: "12-03-2021", measure: "15", comentary: "feeded test data", indicator_id: "2")
+Measure.create(day: "13-05-2021", measure: "17", comentary: "feeded test data", indicator_id: "2")
+Measure.create(day: "14-05-2021", measure: "21", comentary: "feeded test data", indicator_id: "2")
+Measure.create(day: "15-05-2021", measure: "23", comentary: "feeded test data", indicator_id: "2")
+Measure.create(day: "16-05-2021", measure: "18", comentary: "feeded test data", indicator_id: "2")
+Measure.create(day: "17-05-2021", measure: "14", comentary: "feeded test data", indicator_id: "2")
+
+
+
+#Indicator.create(name: 'Sales Visits', goal: 5)
+
+Measure.create(day: "20-04-2021", measure: "10", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "21-04-2021", measure: "17", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "22-04-2021", measure: "17", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "23-04-2021", measure: "20", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "24-04-2021", measure: "21", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "25-04-2021", measure: "3", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "26-04-2021", measure: "3", comentary: "feeded test data", indicator_id: "3")
+
+Measure.create(day: "03-01-2021", measure: "21", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "04-01-2021", measure: "15", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "05-01-2021", measure: "17", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "06-01-2021", measure: "21", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "07-01-2021", measure: "23", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "08-01-2021", measure: "18", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "09-01-2021", measure: "14", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "10-02-2021", measure: "21", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "12-02-2021", measure: "15", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "13-02-2021", measure: "17", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "14-02-2021", measure: "21", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "15-02-2021", measure: "23", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "16-02-2021", measure: "18", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "17-02-2021", measure: "14", comentary: "feeded test data", indicator_id: "3")
+
+Measure.create(day: "03-05-2021", measure: "21", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "04-05-2021", measure: "15", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "05-05-2021", measure: "17", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "06-05-2021", measure: "21", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "07-05-2021", measure: "23", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "08-05-2021", measure: "18", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "09-05-2021", measure: "14", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "10-05-2021", measure: "21", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "12-05-2021", measure: "15", comentary: "feeded test data", indicator_id: "3")
+Measure.create(day: "13-05-2021", measure: "17", comentary: "feeded test data", indicator_id: "3")
+
+
+#Indicator.create(name: 'Demo Meeting', goal: 5)
+
+Measure.create(day: "03-05-2021", measure: "21", comentary: "feeded test data", indicator_id: "4")
+Measure.create(day: "04-05-2021", measure: "15", comentary: "feeded test data", indicator_id: "4")
+Measure.create(day: "05-05-2021", measure: "17", comentary: "feeded test data", indicator_id: "4")
+Measure.create(day: "06-05-2021", measure: "21", comentary: "feeded test data", indicator_id: "4")
+Measure.create(day: "07-05-2021", measure: "23", comentary: "feeded test data", indicator_id: "4")
+Measure.create(day: "08-05-2021", measure: "18", comentary: "feeded test data", indicator_id: "4")
+Measure.create(day: "09-05-2021", measure: "14", comentary: "feeded test data", indicator_id: "4")
+Measure.create(day: "10-05-2021", measure: "21", comentary: "feeded test data", indicator_id: "4")
+Measure.create(day: "12-05-2021", measure: "15", comentary: "feeded test data", indicator_id: "4")
+Measure.create(day: "13-05-2021", measure: "17", comentary: "feeded test data", indicator_id: "4")
+Measure.create(day: "14-05-2021", measure: "21", comentary: "feeded test data", indicator_id: "4")
+Measure.create(day: "15-05-2021", measure: "23", comentary: "feeded test data", indicator_id: "4")
+Measure.create(day: "16-05-2021", measure: "18", comentary: "feeded test data", indicator_id: "4")
+Measure.create(day: "17-05-2021", measure: "14", comentary: "feeded test data", indicator_id: "4")
+
+
+#Indicator.create(name: 'Qoutes Issued', goal: 20)
+
+Measure.create(day: "03-02-2021", measure: "21", comentary: "feeded test data", indicator_id: "5")
+Measure.create(day: "04-02-2021", measure: "15", comentary: "feeded test data", indicator_id: "5")
+Measure.create(day: "05-02-2021", measure: "17", comentary: "feeded test data", indicator_id: "5")
+Measure.create(day: "06-02-2021", measure: "21", comentary: "feeded test data", indicator_id: "5")
+Measure.create(day: "07-02-2021", measure: "23", comentary: "feeded test data", indicator_id: "5")
+Measure.create(day: "08-02-2021", measure: "18", comentary: "feeded test data", indicator_id: "5")
+Measure.create(day: "09-02-2021", measure: "14", comentary: "feeded test data", indicator_id: "5")
+
+
+Measure.create(day: "10-03-2021", measure: "10", comentary: "feeded test data", indicator_id: "5")
+Measure.create(day: "11-03-2021", measure: "5", comentary: "feeded test data", indicator_id: "5")
+Measure.create(day: "12-03-2021", measure: "7", comentary: "feeded test data", indicator_id: "5")
+Measure.create(day: "13-03-2021", measure: "20", comentary: "feeded test data", indicator_id: "5")
+Measure.create(day: "15-03-2021", measure: "17", comentary: "feeded test data", indicator_id: "5")
+Measure.create(day: "16-03-2021", measure: "18", comentary: "feeded test data", indicator_id: "5")
+Measure.create(day: "17-03-2021", measure: "15", comentary: "feeded test data", indicator_id: "5")
+
+
+Measure.create(day: "20-04-2021", measure: "10", comentary: "feeded test data", indicator_id: "5")
+Measure.create(day: "21-04-2021", measure: "17", comentary: "feeded test data", indicator_id: "5")
+Measure.create(day: "22-04-2021", measure: "17", comentary: "feeded test data", indicator_id: "5")
+Measure.create(day: "23-04-2021", measure: "20", comentary: "feeded test data", indicator_id: "5")
+Measure.create(day: "24-04-2021", measure: "21", comentary: "feeded test data", indicator_id: "5")
+Measure.create(day: "25-04-2021", measure: "2", comentary: "feeded test data", indicator_id: "5")
+
+
+# AdminUser.create!(email: 'admin@example.com', password: 'password',
+#Mesure                    password_confirmation: 'password')
+
